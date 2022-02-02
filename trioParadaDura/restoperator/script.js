9942f26270a6fb0dbd38369bade26a8e72f055d6
Gustavo Caldas <lgustavorcaldas@gmail.ccom>
2022-02-02 12:23:11 -0300

    up

--- trioParadaDura/restoperator/script.js
+++ trioParadaDura/restoperator/script.js
@@ -4,6 +4,6 @@ function reciver(...add){
     return teste.push(...add)
 }
 
-reciver(4,3,2,1,3,41,53,82,11,33,4,3,2,1,3,41,53,82,11,33)
+reciver([4,5],3,2,1,3,41,53,82,11,33,4,3,2,1,3,41,53,82,11,33)
 
 console.log(teste)
